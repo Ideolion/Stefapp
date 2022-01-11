b7cebbb63e80600fc2e003547213f3051483a2a2
Ideolion <skegul24@gmail.com>
2022-01-11 20:34:54 +0300

    add audio adapter

--- app/src/main/java/com/example/stefapp/ui/Detox/DetoxAdapter.java
+++ app/src/main/java/com/example/stefapp/ui/Detox/DetoxAdapter.java
@@ -1,22 +1,59 @@
 package com.example.stefapp.ui.Detox;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class DetoxAdapter extends RecyclerView.Adapter<DetoxAdapter.DetoxAdapterViewHolder > {
+import com.example.stefapp.R;
 
+import java.util.List;
+
+public class DetoxAdapter extends RecyclerView.Adapter<DetoxAdapter.DetoxAdapterViewHolder > {
+    private int selectedPosition;
+    Context context;
+    List<GetAudio> arrayListAudio;
+    private RecyclerItemClickListener listener;
+
+    public DetoxAdapter(Context context, List<GetAudio> arrayListAudio, RecyclerItemClickListener listener) {
+        this.context = context;
+        this.arrayListAudio = arrayListAudio;
+        this.listener = listener;
+    }
 
     @NonNull
     @Override
     public DetoxAdapter.DetoxAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.audio_item,parent,false);
+        return new DetoxAdapterViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull DetoxAdapter.DetoxAdapterViewHolder holder, int position) {
+        GetAudio getAudio = arrayListAudio.get(position);
+       if(getAudio != null){
+           if (selectedPosition == position){
+               holder.itemView.setBackgroundColor(ContextCompat.getColor(context,R.color.menu_color4));
+
+           }else {
+               holder.itemView.setBackgroundColor(ContextCompat.getColor(context,R.color.menu_color2));
+
+           }
+       }
+
+
+
+
+
+
+        holder.textViewAudioTitle.setText(getAudio.getName());
+       holder.bind(getAudio,listener);
+
 
     }
 
@@ -26,8 +63,25 @@ public class DetoxAdapter extends RecyclerView.Adapter<DetoxAdapter.DetoxAdapter
     }
 
     public class DetoxAdapterViewHolder extends RecyclerView.ViewHolder{
+        private TextView textViewAudioTitle;
+
+
+
         public DetoxAdapterViewHolder(@NonNull View itemView) {
             super(itemView);
+            textViewAudioTitle = itemView.findViewById(R.id.textViewAudioTitle);
+
         }
+
+        public void bind(GetAudio getAudio, RecyclerItemClickListener listener) {
+
+
+
+        }
+    }
+
+    public interface RecyclerItemClickListener {
+
+
     }
 }
---
+++ app/src/main/java/com/example/stefapp/ui/Detox/GetAudio.java
@@ -0,0 +1,50 @@
+package com.example.stefapp.ui.Detox;
+
+public class GetAudio {
+    private String topic;
+    private String chapter;
+    private String name;
+    private String audioUrl;
+
+    public GetAudio() {
+    }
+
+    public GetAudio(String topic, String chapter, String name, String audioUrl) {
+        this.topic = topic;
+        this.chapter = chapter;
+        this.name = name;
+        this.audioUrl = audioUrl;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(String chapter) {
+        this.chapter = chapter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAudioUrl() {
+        return audioUrl;
+    }
+
+    public void setAudioUrl(String audioUrl) {
+        this.audioUrl = audioUrl;
+    }
+}
