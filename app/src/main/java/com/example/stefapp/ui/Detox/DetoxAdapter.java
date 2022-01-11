a03b6d7eb5b6fa369b3f9753e9d25dcc8a7b5753
Ideolion <skegul24@gmail.com>
2022-01-11 15:32:56 +0300

    add audio adapter

--- app/src/main/java/com/example/stefapp/ui/Detox/DetoxAdapter.java
+++ app/src/main/java/com/example/stefapp/ui/Detox/DetoxAdapter.java
@@ -1,6 +1,33 @@
 package com.example.stefapp.ui.Detox;
 
-public class DetoxAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DetoxAdapter extends RecyclerView.Adapter<DetoxAdapter.DetoxAdapterViewHolder > {
 
 
+    @NonNull
+    @Override
+    public DetoxAdapter.DetoxAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DetoxAdapter.DetoxAdapterViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class DetoxAdapterViewHolder extends RecyclerView.ViewHolder{
+        public DetoxAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
